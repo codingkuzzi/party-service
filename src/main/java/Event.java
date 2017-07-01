c1004a914a314593841116a9673443bb058bcf02
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-06-30 23:18:20 -0700

    add consructor and twotests for getGestCount method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,20 @@
 
 public class Event {
+    private int mGuestCount;
+
+    public Event() {
+        mGuestCount = 0;
+
+
+    }
+
+    public Event(int guestCount){
+        mGuestCount = guestCount;
+    }
+
+
+
+    public int getGuestCount(){
+        return mGuestCount;
+    }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -2,9 +2,23 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EventTest {
+
+
+
     @Test
-    public void newEvent_instantiatesCorrectly() {
+    public void newEvent_instantiatesCorrectly_1() {
         Event testEvent = new Event();
         assertEquals(true, testEvent instanceof Event);
     }
+    @Test
+    public void newEvent_getGuestCount_2() {
+        Event testEvent = new Event(2);
+        assertEquals(2, testEvent.getGuestCount());
+    }
+    @Test
+    public void newEvent_getGuestCount_3() {
+        Event testEvent = new Event();
+        assertEquals(0, testEvent.getGuestCount());
+    }
+
 }
